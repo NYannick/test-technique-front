3c3c82e0958afcb6f12df7792aba4b7bc3116fb2
Yannick Noël <>
2019-11-15 00:49:16 +0100

    Add Infinite React Scroller - Need some refacto for getMoviesDiscover and part auth

--- src/containers/ListMovies/ListMovies.tsx
+++ src/containers/ListMovies/ListMovies.tsx
@@ -13,6 +13,8 @@ import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import MenuItem from '@material-ui/core/MenuItem'
 import Close from '@material-ui/icons/Close'
+import InfiniteScroll from 'react-infinite-scroller'
+import axios from 'axios'
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,7 +31,9 @@ const useStyles = makeStyles((theme: Theme) =>
 const ListMovies = (props: any) => {
     const [isLoading, setIsLoading] = useState(false)
     const [movies, setMovies] = useState<any[]>([])
-    const [params, setParams] = useState('')
+    const [sortBy, setSortBy] = useState('')
+    const [page, setPage] = useState(1)
+    const [hasMoreItems, setHasMoreItems] = useState(false)
     const user = JSON.parse(localStorage.getItem('user') || '')
     const classes = useStyles()
 
@@ -40,11 +44,11 @@ const ListMovies = (props: any) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    // refacto
     useEffect(() => {
-        setIsLoading(true)
-        props.dispatch(getMoviesDiscover(params))
+        props.dispatch(getMoviesDiscover(sortBy, 1))
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [params])
+    }, [sortBy])
 
     useEffect(() => {
         if (props.movies) {
@@ -55,8 +59,28 @@ const ListMovies = (props: any) => {
         }
     }, [props.movies])
 
+    // refacto
+    useEffect(() => {
+        if (page === 1)
+            callGetMovieDiscover()
+        return () => {
+            setIsLoading(false)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page])
+
+    // refacto - for now i don't update store
+    const callGetMovieDiscover = () => {
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=8bc50bb087dba9d92cc48b7404da984a&page=${page}&sort_by=${sortBy ? sortBy : 'popularity.desc'}`)
+            .then(res => {
+                setMovies([...movies, ...res.data.results])
+                setHasMoreItems(res.data.results.length >= 20)
+                setPage(page + 1)
+            })
+    }
+
     const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setParams(event.target.value as string)
+        setSortBy(event.target.value as string)
     }
 
     const renderMovies = () => {
@@ -79,16 +103,29 @@ const ListMovies = (props: any) => {
                     <Select
                         labelId="simple-select-label"
                         id="simple-select"
-                        value={params}
+                        value={sortBy}
                         onChange={handleChange}
                     >
                         <MenuItem value='release_date.asc'>Asc</MenuItem>
                         <MenuItem value='release_date.desc'>Desc</MenuItem>
                     </Select>
                 </FormControl>
-                { params && <Close className='close' fontSize='large' onClick={() => setParams('')} /> }
+                { sortBy && <Close className='close' fontSize='large' onClick={() => setSortBy('')} /> }
             </div>
-            { isLoading ? <Spinner /> : renderMovies() }
+            {
+                isLoading
+                    ? <Spinner />
+                    : (
+                        <InfiniteScroll
+                            pageStart={0}
+                            loadMore={callGetMovieDiscover}
+                            hasMore={hasMoreItems}
+                            loader={<Spinner key='infinite-scroll-loader' />}
+                        >
+                            {renderMovies()}
+                        </InfiniteScroll>
+                    )
+            }
         </div>
     )
 }
--- src/store/actions/movies.ts
+++ src/store/actions/movies.ts
@@ -9,13 +9,13 @@ import axios from 'axios'
 const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmM1MGJiMDg3ZGJhOWQ5MmNjNDhiNzQwNGRhOTg0YSIsInN1YiI6IjVkYzQzMjQ4OGQyMmZjMDAxNDNlMmFiZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XRBKBn2cnFW_HJP7oJlsXMfWAn9-V3_rx3nT1chvPIQ'
 const api_key = '8bc50bb087dba9d92cc48b7404da984a'
 
-export const getMoviesDiscover = (params: string) => {
+export const getMoviesDiscover = (sortBy: string, page: number) => {
     return (dispatch: any) => {
         const options = requestOptions(
             'get',
             '/3/discover/movie',
             accessToken,
-            { api_key, sort_by: params ? params : 'popularity.desc' }
+            { api_key, sort_by: sortBy ? sortBy : 'popularity.desc', page }
         )
         axios({ ...options, headers: {} })
             .then(res => {
